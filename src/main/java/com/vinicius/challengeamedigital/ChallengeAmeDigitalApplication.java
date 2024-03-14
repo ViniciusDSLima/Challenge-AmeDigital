b6ba6a7ee2d8ecde4e97d2959204e6452d098c93
Vinicius <vinicius.devjvm@gmail.com>
2024-03-14 17:59:29 -0300

    feat: initial commit.

---
+++ src/main/java/com/vinicius/challengeamedigital/ChallengeAmeDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.challengeamedigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAmeDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeAmeDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinicius/challengeamedigital/ChallengeAmeDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.challengeamedigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAmeDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
